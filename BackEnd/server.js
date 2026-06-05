c3af2c82be028747d580ff7b90b0317da6501094
silambarasanstr <simbube2013@gmail.com>
2026-06-06 00:05:08 +0530

    new update model

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -72,7 +72,7 @@ const startServer = async () => {
     await connectDB();
 
     app.listen(PORT, () => {
-      console.log(`Server is running on port: ${PORT}`);
+      console.log(`Server is running on port...: ${PORT}`);
     });
   } catch (error) {
     console.log("Server failed:", error.message);
